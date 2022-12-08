e1d4c19a689194bb7084137d52005c81baae62f7
Petchesi-Iulian <iamnotacat12@gmail.com>
2022-12-08 18:22:28 +0200

    Add indexers for publisher/organisations.

--- eea/coremetadata/upgrades/to_24.py
+++ eea/coremetadata/upgrades/to_24.py
@@ -19,85 +19,118 @@ from eea.coremetadata import EEAMessageFactory as _
 logger = logging.getLogger("eea.coremetadata.upgrade")
 
 
+taxonomy_args = [
+    {
+        "name": "topics",
+        "title": "EEA Coremetadata Topics taxonomy",
+        "field_title": "Topics",
+        "field_prefix": "",
+        "description": "Topic selected from a predefined list for eea.coremetadata",
+        "field_description": "Topics selected from a predefined list",
+        "taxonomy_fieldset": "default",
+        "default_language": "en"
+    },
+    {
+        "name": "publisher",
+        "title": "EEA Coremetadata Publisher taxonomy",
+        "field_title": "Publisher",
+        "field_prefix": "",
+        "description": "Publishers selected from a predefined list for eea.coremetadata",
+        "field_description": "Publishers selected from a predefined list",
+        "taxonomy_fieldset": "ownership",
+        "default_language": "en"
+    },
+    {
+        "name": "other_organisations",
+        "title": "EEA Coremetadata Organisations taxonomy",
+        "field_title": "Organisations",
+        "field_prefix": "",
+        "description": "Organisations selected from a predefined list for eea.coremetadata",
+        "field_description": "Organisations selected from a predefined list",
+        "taxonomy_fieldset": "ownership",
+        "default_language": "en"
+    }
+]
+
+
 def to_24(context):
     """ Add behaviors for core metadata taxonomies """
-    new_args = {}
-    new_args["name"] = "topics"
-    new_args["title"] = "EEA Coremetadata Topics taxonomy"
-    new_args["field_title"] = "Topics"
-    new_args["field_prefix"] = ""
-    new_args["description"] = "Topic selected from a predefined list for eea.coremetadata"
-    new_args["field_description"] = new_args["description"]
-    new_args["taxonomy_fieldset"] = "default"
-    new_args["default_language"] = "en"
-
     sm = context.aq_parent.getSiteManager()
 
-    behavior = TaxonomyBehavior(**new_args)
+    for new_args in taxonomy_args:
+        behavior = TaxonomyBehavior(**new_args)
 
-    sm.registerUtility(behavior, IBehavior, name=new_args["name"])
+        sm.registerUtility(behavior, IBehavior, name=new_args["name"])
 
-    new_args["vocabulary_name"] = 'collective.taxonomy.eeatopicstaxonomy'
-    new_args["short_name"] = "topics"
-    new_args["field_name"] = (new_args['field_prefix'] or "") + new_args['short_name']
+    taxonomy_args[0]["vocabulary_name"] = 'collective.taxonomy.eeatopicstaxonomy'
+    taxonomy_args[0]["short_name"] = "topics"
+    taxonomy_args[0]["field_name"] = (taxonomy_args[0]['field_prefix'] or "") + taxonomy_args[0]['short_name']
 
+    taxonomy_args[1]["vocabulary_name"] = 'collective.taxonomy.eeapublishertaxonomy'
+    taxonomy_args[1]["short_name"] = "publisher"
+    taxonomy_args[1]["field_name"] = (taxonomy_args[1]['field_prefix'] or "") + taxonomy_args[1]['short_name']
 
-    sm.registerAdapter(
-        TaxonomyIndexer(new_args['field_name'], new_args['vocabulary_name']),
-        (IDexterityContent, IZCatalog),
-        IIndexer,
-        name=new_args['field_name'],
-    )
+    taxonomy_args[2]["vocabulary_name"] = 'collective.taxonomy.eeaorganisationstaxonomy'
+    taxonomy_args[2]["short_name"] = "other_organisations"
+    taxonomy_args[2]["field_name"] = (taxonomy_args[2]['field_prefix'] or "") + taxonomy_args[2]['short_name']
 
-    catalog = getToolByName(context.aq_parent, "portal_catalog")
-
-    try:
-        catalog.delIndex("topics")
-        catalog.delColumn("topics")
-    except CatalogError:
-        logging.info(
-            "Index {0} doesn't exists".format(new_args['field_name'])  # noqa: E501
+    for new_args in taxonomy_args:
+        sm.registerAdapter(
+            TaxonomyIndexer(new_args['field_name'], new_args['vocabulary_name']),
+            (IDexterityContent, IZCatalog),
+            IIndexer,
+            name=new_args['field_name'],
         )
 
-    idx_object = KeywordIndex(str(new_args['field_name']))
-    try:
-        catalog.addIndex(new_args['field_name'], idx_object)
-    except CatalogError:
-        logging.info(
-            "Index {0} already exists, we hope it is proper configured".format(
-                new_args['field_name']
+        catalog = getToolByName(context.aq_parent, "portal_catalog")
+
+        try:
+            catalog.delIndex(new_args['field_name'])
+            catalog.delColumn(new_args['field_name'])
+        except CatalogError:
+            logging.info(
+                "Index {0} doesn't exists".format(new_args['field_name'])  # noqa: E501
+            )
+
+        idx_object = KeywordIndex(str(new_args['field_name']))
+        try:
+            catalog.addIndex(new_args['field_name'], idx_object)
+        except CatalogError:
+            logging.info(
+                "Index {0} already exists, we hope it is proper configured".format(
+                    new_args['field_name']
+                )  # noqa: E501
+            )
+
+        try:
+            catalog.addColumn(new_args['field_name'])
+        except CatalogError:
+            logging.info(
+                "Column {0} already exists".format(new_args['field_name'])
             )  # noqa: E501
+
+        registry = getUtility(IRegistry)
+        prefix = "plone.app.querystring.field." + new_args['field_name']
+
+        def add(name, value):
+            registry.records[prefix + "." + name] = value
+
+        add("title", Record(field.TextLine(), safe_unicode(new_args['field_title'])))
+        add("enabled", Record(field.Bool(), True))
+        add("group", Record(field.TextLine(), safe_unicode("Taxonomy")))
+        add(
+            "operations",
+            Record(
+                field.List(value_type=field.TextLine()),
+                ["plone.app.querystring.operation.selection.is"],
+            ),
         )
-
-    try:
-        catalog.addColumn(new_args['field_name'])
-    except CatalogError:
-        logging.info(
-            "Column {0} already exists".format(new_args['field_name'])
+        add(
+            "vocabulary", Record(field.TextLine(), safe_unicode(new_args['vocabulary_name']))
         )  # noqa: E501
-
-    registry = getUtility(IRegistry)
-    prefix = "plone.app.querystring.field." + new_args['field_name']
-
-    def add(name, value):
-        registry.records[prefix + "." + name] = value
-
-    add("title", Record(field.TextLine(), safe_unicode(new_args['field_title'])))
-    add("enabled", Record(field.Bool(), True))
-    add("group", Record(field.TextLine(), safe_unicode("Taxonomy")))
-    add(
-        "operations",
-        Record(
-            field.List(value_type=field.TextLine()),
-            ["plone.app.querystring.operation.selection.is"],
-        ),
-    )
-    add(
-        "vocabulary", Record(field.TextLine(), safe_unicode(new_args['vocabulary_name']))
-    )  # noqa: E501
-    add("fetch_vocabulary", Record(field.Bool(), True))
-    add("sortable", Record(field.Bool(), False))
-    add("description", Record(field.Text(), safe_unicode("")))
+        add("fetch_vocabulary", Record(field.Bool(), True))
+        add("sortable", Record(field.Bool(), False))
+        add("description", Record(field.Text(), safe_unicode("")))
 
 
 # (Pdb) pp vars(self)
