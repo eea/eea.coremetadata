97c90e4ac83658f1f0e24085adf333737c1fa738
David Ichim <ichim.david@gmail.com>
2024-09-09 17:33:29 +0300

    Lint fix 2

--- eea/coremetadata/patches/pac_metadata.py
+++ eea/coremetadata/patches/pac_metadata.py
@@ -1,14 +1,23 @@
+"""
+This module patches the plone.app.dexterity.behaviors.metadata.IOwnership
+to use the plone.app.vocabularies.Principals vocabulary for creators and
+contributors.
+"""
 from logging import getLogger
 
 
 def install_pac_metadata():
+    """
+    Patch the IOwnership tagged value to use the Principals vocabulary
+    for creators and contributors fields.
+    """
     from plone.autoform.interfaces import WIDGETS_KEY
     from plone.app.dexterity.behaviors.metadata import IOwnership
 
     tgv = IOwnership.getTaggedValue(WIDGETS_KEY)
-    tgv["creators"].params = {"vocabulary": 
+    tgv["creators"].params = {"vocabulary":
                               "plone.app.vocabularies.Principals"}
-    tgv["contributors"].params = {"vocabulary": 
+    tgv["contributors"].params = {"vocabulary":
                                   "plone.app.vocabularies.Principals"}
 
 
